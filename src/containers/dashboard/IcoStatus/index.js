dc51713d1a37ab3409a4f8ce2ecbade9c1e7b6c6
ZEON Network <37929096+ZeonNetwork@users.noreply.github.com>
2018-11-18 17:56:25 +0200

    Update index.js

--- src/containers/dashboard/IcoStatus/index.js
+++ src/containers/dashboard/IcoStatus/index.js
@@ -23,7 +23,7 @@ const IcoStatus = (props) => {
     <Callout title={t('icoStatus.title')}>
       <Block
         label={t('icoStatus.price')}
-        value={`${bigNum(tokenPrice.ETH, 4)} ETH / ${bigNum(tokenPrice.USD, 6)}$`}
+        value={`/* ${bigNum(tokenPrice.ETH, 4)} ETH */ / ${bigNum(tokenPrice.USD, 6)}$`}
         fetching={fetching}/>
 
       <Block
